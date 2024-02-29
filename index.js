2d0407761a25db94b6e46e85359586c839b5a75b
maundu55 <marymaundu.mm@gmail.com>
2024-02-29 23:48:17 +0400

    add more js

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-function palindrome(str){
-//convert the string into an array using the string.split function
-
-const str = string.split('');
-
-//use reverse()method to reverse the array values
-const revValue = str.reverse();
-
-//use join() method to group the array values into the string
-
-const d = revValue.join('');
-
-if (str == d){
- document.getElementById("pa2").value = b + " is a Palindrome String";
-}
-else
-document.getElementById("pa2").value = b + " is not a Palindrome String";
-}
+function palindromeFun (str )  
+{  
+// convert the string into an array using the string.split() function  
+const arrValue = string.split (''); //   
+  
+// use reverse() method to reverse the array values  
+const reveArrVal = arrValue.reverse();   
+  
+// use join() method to group the array values into the string  
+const revString = reveArrVal.join('');  
+  
+if (string == revString) // if string condition is equal to the revString  
+{  
+alert('It is a Palindrome string '); // print the Palindrome   
+}  
+else {  
+alert (' It is not a Palindrome string' ); // if the condition is not true.  
+}  
+}  
+// take a string from the user  
+const string = prompt( ' Enter the string to check Palindrome ');  
+const value = palindromeFun (string); // call the function  
+console.log(value); 
